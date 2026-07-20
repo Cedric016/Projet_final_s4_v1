9f1d8bb0ef02f9406f9dde5908b2a6facf101460
Cedric016 <mamitiana.rafeno@gmail.com>
2026-07-20 12:20:45 +0300

    Redirection home vers index.php/dashboard et serve.bat ouvre cette URL

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,6 +6,6 @@ class Home extends BaseController
 {
     public function index()
     {
-        return redirect()->to(site_url('dashboard'));
+        return redirect()->to('index.php/dashboard', 302);
     }
 }
--- serve.bat
+++ serve.bat
@@ -2,4 +2,4 @@
 cd /d "%~dp0"
 start "" php spark serve --port 8080
 timeout /t 3 >nul
-start "" http://localhost:8080
+start "" http://localhost:8080/index.php/dashboard
