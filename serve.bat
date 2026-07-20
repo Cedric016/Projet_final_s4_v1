dd5ff9c1eb14e2387d9985a31b84748d32f67890
Cedric016 <mamitiana.rafeno@gmail.com>
2026-07-20 12:35:22 +0300

    serve.bat: port 8081 IPv4 pour eviter conflit avec Apache sur 8080

--- serve.bat
+++ serve.bat
@@ -1,5 +1,5 @@
 @echo off
 cd /d "%~dp0"
-start "" php spark serve --port 8080
+start "" php -S 127.0.0.1:8081 -t public public/index.php
 timeout /t 3 >nul
-start "" http://localhost:8080/index.php/dashboard
+start "" http://127.0.0.1:8081/index.php/dashboard
